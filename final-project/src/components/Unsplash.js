b40d322e7a1ba4c4f1f78d75a67dd80885ae2ba6
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-05-16 22:48:21 -0400

    remove private unsplash api, using public unsplash api, gave to-do container a max-width and added scrollbar instead.

--- final-project/src/components/Unsplash.js
+++ final-project/src/components/Unsplash.js
@@ -1,26 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const Unsplash = () => {
 
-  const [img, setImg] = useState('');
-
-  const API = 'https://api.unsplash.com/photos/random/?client_id='
-  const KEY = 'O-TGWpqjLoj5Upmat-aYCuaB3bPKDxmib5mUwdwl8oU'
-
   useEffect(() => {
-    fetch(`${API}${KEY}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-
-        setImg(data.urls.raw);
-        document.body.style.backgroundImage = `url(${img})`;
-      })
+    document.body.style.backgroundImage = 'url(https://source.unsplash.com/random'
   }, [])
 
   return (
     <>
-      <div className="image-credits">Image by Unsplash</div>
+      <div className="image-credits">Image by <a href="https://unsplash.com/" target="_blank" rel="noopener noreferrer">Unsplash</a></div>
     </>
   )
     ;
